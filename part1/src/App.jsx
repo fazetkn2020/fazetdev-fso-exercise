fbfc375ca5364f8ed8453704b321048690b10d8c
fazetdev <fazetdev@gmail.com>
2025-12-04 19:32:43 +0100

    exercise 1.12 anecdote

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -75,29 +75,57 @@ const Statistics = ({ good, neutral, bad }) => {
   )
 }
 
-const App = () => {  
-  const [good, setGood] = useState(0)  
-  const [neutral, setNeutral] = useState(0)  
-  const [bad, setBad] = useState(0)  
+const App = () => {
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const addGood = () => setGood(good + 1)  
-  const addNeutral = () => setNeutral(neutral + 1)  
-  const addBad = () => setBad(bad + 1)  
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
 
-  return (  
-    <div>  
-      <CourseInfo />  
+  
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-      {/* 1.6+ starts here */}  
-      <h1>give feedback</h1>  
-      <button onClick={addGood}>good</button>  
-      <button onClick={addNeutral}>neutral</button>  
-      <button onClick={addBad}>bad</button>  
+  const [selected, setSelected] = useState(0)
 
-      
+  const randomAnecdote = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random)
+  }
+
+  return (
+    <div>
+      {/* 1.1–1.5 */}
+      <CourseInfo />
+      <hr />
+
+      {/* 1.6–1.11 Unicafe */}
+      <h1>give feedback</h1>
+      <button onClick={addGood}>good</button>
+      <button onClick={addNeutral}>neutral</button>
+      <button onClick={addBad}>bad</button>
+
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>  
-  )  
-}  
+      <hr />
+
+      {/* 1.12 Anecdotes */}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={randomAnecdote}>next anecdote</button>
+    </div>
+  )
+}
 
 export default App
