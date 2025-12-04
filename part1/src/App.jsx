1fb6336e099805549002bb6215a75cb4f2f23323
fazetdev <fazetdev@gmail.com>
2025-12-04 17:06:05 +0100

    exerciae 1.6 unicafe beginning

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react'
+
 const Header = (props) => {
   return <h1>{props.course}</h1>
 }
-
 
 const Part = (props) => {
   return <p>{props.part.name} {props.part.exercises}</p>
@@ -24,7 +25,7 @@ const Total = (props) => {
   return <p>Number of exercises {total}</p>
 }
 
-const App = () => {
+const CourseInfo = () => {
   const course = 'Half Stack application development'
   const parts = [
     { name: 'Fundamentals of React', exercises: 10 },
@@ -37,6 +38,34 @@ const App = () => {
       <Header course={course} />
       <Content parts={parts} />
       <Total parts={parts} />
+      <hr />
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <CourseInfo />
+
+      {/* 1.6+ starts here */}
+      <h1>give feedback</h1>
+      <button onClick={addGood}>good</button>
+      <button onClick={addNeutral}>neutral</button>
+      <button onClick={addBad}>bad</button>
+
+      <h1>statistics</h1>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad}
     </div>
   )
 }
