0306d7562f685ebf57c9ea93b43ac291f0c10158
fazetdev <fazetdev@gmail.com>
2025-12-06 16:23:21 +0100

    exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,45 +1,17 @@
-import { useState } from 'react'
+const addPerson = (event) => {
+  event.preventDefault()
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+  if (persons.find(p => p.name === newName)) {
+    alert(newName + ' is already added to phonebook')
+    return
   }
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {persons.map(person => 
-          <p key={person.name}>{person.name} {person.number}</p>
-        )}
-      </div>
-    </div>
-  )
-}
+  const personObject = {
+    name: newName,
+    number: newNumber
+  }
 
-export default App
+  setPersons(persons.concat(personObject))
+  setNewName('')
+  setNewNumber('')
+}
