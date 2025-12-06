40e0290c3f4d6c847872eb2d4a32f8597e154fb5
fazetdev <fazetdev@gmail.com>
2025-12-06 19:35:23 +0100

    Complete phonebook exercises up to 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -10,29 +7,23 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
-  // Load initial data from server
+  // Load data from backend when app starts
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    personService.getAll().then(initialPersons => setPersons(initialPersons))
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
-
     if (persons.find(p => p.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
-
     const personObject = { name: newName, number: newNumber }
-
-    // Save to backend
-    axios.post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
+    personService.create(personObject).then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson))
+      setNewName('')
+      setNewNumber('')
+    })
   }
 
   const personsToShow = filter === ''
@@ -42,19 +33,21 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={(e) => setFilter(e.target.value)} />
+      <div>
+        filter shown with: <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
 
       <h3>Add a new</h3>
-      <PersonForm
-        newName={newName}
-        newNumber={newNumber}
-        handleNameChange={(e) => setNewName(e.target.value)}
-        handleNumberChange={(e) => setNewNumber(e.target.value)}
-        handleSubmit={addPerson}
-      />
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={(e) => setNewName(e.target.value)} /></div>
+        <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+        <div><button type="submit">add</button></div>
+      </form>
 
       <h3>Numbers</h3>
-      <Persons persons={personsToShow} />
+      {personsToShow.map(person => 
+        <p key={person.id}>{person.name} {person.number}</p>
+      )}
     </div>
   )
 }
