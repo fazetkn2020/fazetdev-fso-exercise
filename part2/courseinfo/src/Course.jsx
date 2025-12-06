85ccee09a3598a3e9d1dab9f3b73e3cc5d8f6bd4
fazetdev <fazetdev@gmail.com>
2025-12-06 15:12:35 +0100

    add Courseinfo part2 and aolution of exercise 2.1

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,22 @@
+const Header = (props) => <h1>{props.name}</h1>
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Content = (props) => (
+  <div>
+    {props.parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Course = (props) => (
+  <div>
+    <Header name={props.course.name} />
+    <Content parts={props.course.parts} />
+  </div>
+)
