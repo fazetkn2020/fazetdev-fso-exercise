8f5bd714ca4e99148a9730126060ddac5a8a0e75
fazetdev <fazetdev@gmail.com>
2025-12-06 16:26:24 +0100

    full exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,52 @@
-const addPerson = (event) => {
-  event.preventDefault()
+import { useState } from 'react'
 
-  if (persons.find(p => p.name === newName)) {
-    alert(newName + ' is already added to phonebook')
-    return
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (persons.find(p => p.name === newName)) {
+      alert(newName + ' is already added to phonebook')
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
   }
 
-  const personObject = {
-    name: newName,
-    number: newNumber
-  }
-
-  setPersons(persons.concat(personObject))
-  setNewName('')
-  setNewNumber('')
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+          <p key={person.name}>{person.name} {person.number}</p>
+        )}
+      </div>
+    </div>
+  )
 }
+
+export default App
