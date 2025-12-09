1a94b6e52f0b613ae40a7b7eb312b6013ba0d9f1
Fazet FSO <fazetkn2020@gmail.com>
2025-12-09 18:01:02 +0100

    install morgan and answer exercise 3.8* wow

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,9 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny')) 
+
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : ' ')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { id: 1, name: 'Arto Hellas', number: '040-123456' },
@@ -17,50 +19,35 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  const total = persons.length
-  const now = new Date()
-  res.send(`<div>Phonebook has info for ${total} people</div><div>${now.toString()}</div>`)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).json({ error: 'Person not found' })
-  }
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  if (persons.some(p => p.id === id)) {
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
-  } else {
-    res.status(404).end()
-  }
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>Server Time: ${new Date()}</p>
+    `)
 })
 
 app.post('/api/persons', (req, res) => {
   const { name, number } = req.body
-
+  
   if (!name || !number) {
     return res.status(400).json({ error: 'name or number is missing' })
   }
-
   if (persons.some(p => p.name === name)) {
     return res.status(400).json({ error: 'name must be unique' })
   }
 
   const person = {
-    id: Math.floor(Math.random() * 1000000),
+    id: Math.floor(Math.random() * 1000000), 
     name,
     number
   }
-
   persons.push(person)
   res.status(201).json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id) 
+  res.status(204).end()
 })
 
 const PORT = 3001
