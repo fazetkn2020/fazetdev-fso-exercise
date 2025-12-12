bd59a9930cab5599db7a9684d6d3939df3aefc1a
Fazet FSO <fazetkn2020@gmail.com>
2025-12-12 15:42:10 +0100

    exercise 4.2  modules

--- part4/index.js
+++ part4/index.js
@@ -1,43 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// Use Atlas connection string from .env
 const mongoUrl = process.env.MONGODB_URI
+
 mongoose.connect(mongoUrl, { family: 4 })
   .then(() => console.log('connected to MongoDB'))
-  .catch((err) => console.error('error connection to MongoDB:', err.message))
+  .catch(err => console.error('error connection to MongoDB:', err.message))
 
 app.use(express.json())
+app.use('/api/blogs', blogsRouter) // attach router
 
-// GET all blogs
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-// POST a new blog
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-// Listen on all interfaces for Termux
 const PORT = process.env.PORT || 3003
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
