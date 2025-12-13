59aff1f2e8dfb236b17c1f3ca10d877f033370c6
Fazet FSO <fazetkn2020@gmail.com>
2025-12-13 15:04:00 +0100

    ex4.8 done

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  }
+]
+
+beforeAll(async () => {
+  // Connect to test database
+  await mongoose.connect(process.env.TEST_MONGODB_URI)
+}, 30000) // 30 second timeout
+
+beforeEach(async () => {
+  // Clear the database completely
+  await Blog.deleteMany({})
+  
+  // Save blogs one by one to ensure order
+  for (let blog of initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+describe('GET /api/blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 10000)
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  }, 10000)
+
+  test('blogs have id field instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    // Check that id field exists (Mongoose should convert _id to id)
+    expect(response.body[0].id).toBeDefined()
+  }, 10000)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
